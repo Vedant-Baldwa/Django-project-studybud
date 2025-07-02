e4ef3aac775e8b73e192bc66ee7f14d21575b504
Vedant Baldwa <vedantbaldwa@gmail.com>
2025-07-02 14:24:47 +0530

    2-07-2025

--- base/models.py
+++ base/models.py
@@ -13,18 +13,15 @@ class Topic(models.Model):
     def __str__(self):
         return self.name
 
-
-
 # Create your models here.
 class Room(models.Model):
     host=models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     name=models.CharField(max_length=200)
     topic=models.ForeignKey(Topic,on_delete=models.SET_NULL,null=True)
     description=models.TextField(null=True,blank=True)
-    #participants=
+    participants=models.ManyToManyField(User,related_name='participants',blank=True)
     updated=models.DateTimeField(auto_now=True) #every time it is saved
     created=models.DateTimeField(auto_now_add=True) #only at first save
-
 
     class Meta:
         ordering=['-updated','-created']
@@ -37,6 +34,9 @@ class Message(models.Model):
     body=models.TextField()
     updated=models.DateTimeField(auto_now=True)
     created=models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering=['-updated','-created']
 
     def __str__(self):
         return self.body[0:50]
--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.db.models import Q
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
@@ -20,7 +20,7 @@ def loginPage(request):
     if request.user.is_authenticated:
         return redirect('home')
     if request.method=='POST':
-        username=request.POST.get('username')
+        username=request.POST.get('username').lower()
         password=request.POST.get('password')
         try:
             user=User.objects.get(username=username)
@@ -46,6 +46,17 @@ def logoutUser(request):
 def registerUser(request):
     page='register'
     form=UserCreationForm()
+    if request.method=='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            user=form.save(commit=False)
+            user.username=user.username.lower()
+            user.save()
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,"An error occured during registeration")
+
     return render(request,'base/login_register.html',{'page':page,'form':form})
 def home(request):
     q=request.GET.get('q') if request.GET.get('q')!=None else ''
@@ -56,13 +67,26 @@ def home(request):
         )
     topics=Topic.objects.all()
     room_count=rooms.count()
-    context={'rooms':rooms,'topics':topics,'room_count':room_count}
+    room_messages=Message.objects.filter(Q(room__topic__name__icontains=q))
+
+
+    context={'rooms':rooms,'topics':topics,'room_count':room_count,'room_messages':room_messages}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
     room=Room.objects.get(id=pk)
+    room_messages=room.message_set.all()
+    participants=room.participants.all()
+    if request.method=='POST':
+        message=Message.objects.create(
+                user=request.user,
+                room=room,
+                body=request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room',pk=room.id)
 
-    context={'room':room}
+    context={'room':room,'room_messages':room_messages,'participants':participants}
     
     return render(request,'base/room.html',context)
 
@@ -103,3 +127,16 @@ def deleteRoom(request,pk):
         room.delete()
         return redirect('home')
     return render(request,'base/delete.html',{'obj':room})
+
+
+
+@login_required(login_url='/login')
+def deleteMessage(request,pk):
+    message=Message.objects.get(id=pk)
+
+    if request.user !=message.user:
+        return HttpResponse('You are not allowed here !!')
+    if request.method =='POST':
+        message.delete()
+        return redirect('home')
+    return render(request,'base/delete.html',{'obj':message})
